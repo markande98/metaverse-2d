485a9a7a6c3549902438b1051ba39b2f706378a1
markande98 <gauravbpit1998@gmail.com>
2024-11-04 17:04:55 +0530

    test sign in and sign up endpoints

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -3,6 +3,9 @@ import { router } from "./routes/v1";
 
 const app = express();
 
+app.use(express.json());
 app.use("/api/v1", router);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running....");
+});
--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -2,12 +2,95 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
 import { adminRouter } from "./admin";
+import jwt from "jsonwebtoken";
+import { hash, compare } from "../../script";
+
+import { db } from "@repo/db/client";
+import { signInSchema, signUpSchema } from "../../types";
+import { JWT_SECRET } from "../../config";
 
 export const router = Router();
 
-router.post("/signup", (req, res) => {});
+router.post("/signup", async (req, res) => {
+  const parsedData = signUpSchema.safeParse(req.body);
 
-router.post("/signin", (req, res) => {});
+  if (!parsedData.success) {
+    res.status(400).json({
+      message: "Validation failed",
+    });
+    return;
+  }
+
+  try {
+    const { username, password, type } = parsedData.data;
+
+    const hashedPassword = await hash(password);
+    const user = await db.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role: type === "admin" ? "Admin" : "User",
+      },
+    });
+
+    res.status(200).json({
+      userId: user.id,
+    });
+  } catch (e) {
+    res.status(400).json({
+      message: "User already exists",
+    });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const parsedData = signInSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.status(400).json({
+      message: "Invalid input",
+    });
+    return;
+  }
+
+  try {
+    const { username, password } = parsedData.data;
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      res.status(403).json({
+        message: "User not found",
+      });
+      return;
+    }
+
+    const isValid = await compare(password, user.password);
+    if (!isValid) {
+      res.status(403).json({
+        message: "Invalid Password",
+      });
+      return;
+    }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role,
+      },
+      JWT_SECRET
+    );
+    res.status(200).json({
+      token,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 
 router.get("/avatars", (req, res) => {});
 
